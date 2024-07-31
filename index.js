e1dcfa3792660ce1dbfddec37b918c0aa3597b93
thiateleu <thiateleu@gmail.com>
2024-07-31 01:04:35 -0400

    routes index

--- index.js
+++ index.js
@@ -9,6 +9,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // define the template engine
-app.set("view engine", "js");
+app.set("view engine", "ejs");
 
-//
+//serve static files
+app.use(express.static("styles"));
+
+const customMiddleware = require("./middleware/custom");
+const errorHandler = require("./middleware/errorHandler");
+
+app.use(customMiddleware.logRequestDetails);
+
+///routes
+const postRoustes = require("./routes/posts");
+const userRoustes = require("./routes/users");
+app.use("./posts", postRoustes);
+app.use("./users", userRoustes);
+// Example route
+app.get("/", (req, res) => {
+  res.send("Hello Friends!");
+});
+//error handling
+app.use(errorHandler.handleErrors);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
